6b0ef897a92546879a89fb287c258e9b089f65de
Davi Silva <davisilvaphoto@gmail.com>
2018-12-30 15:15:16 -0300

    Updates

---
+++ precache-manifest.60b64ac1deb9255dccbac53d91350a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8827202ed477b90e3b90",
+    "url": "/my-perfil/static/css/main.70a61835.chunk.css"
+  },
+  {
+    "revision": "8827202ed477b90e3b90",
+    "url": "/my-perfil/static/js/main.8827202e.chunk.js"
+  },
+  {
+    "revision": "f22fde3f0ca9fa1ca6b1",
+    "url": "/my-perfil/static/js/1.f22fde3f.chunk.js"
+  },
+  {
+    "revision": "d33a74dc44c58b48f7cf",
+    "url": "/my-perfil/static/js/runtime~main.d33a74dc.js"
+  },
+  {
+    "revision": "702f3c4cd5839e5747d04611d80e10bd",
+    "url": "/my-perfil/static/media/perfil.702f3c4c.png"
+  },
+  {
+    "revision": "cc4a9b4439366acbbb84fb1667e9ff20",
+    "url": "/my-perfil/index.html"
+  }
+];
