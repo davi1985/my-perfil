9e2db5131137ed209fb28b9618cbcff702797ff0
Davi Silva <davisilvaphoto@gmail.com>
2018-12-30 14:04:10 -0300

    static web page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import github from './github.png';
+import twitter from './twitter.png';
+import medium from './medium.png';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
+          <a href="#" className="App-perfil"></a>
+          <h3>
+            Hi, I'm Davi Silva
+          </h3>
+          <p>Welcome here in my personal Website.</p>
+        
+          <div className="links">
+            <a href="https://github.com/davi1985"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            rel="noopener noreferrer" className="App-links" alt="github"><img src={github} className="App-links"/></a>
+            <a href="https://twitter.com/dssilvap"
+            target="_blank"
+            rel="noopener noreferrer" className="App-links" alt="twitter"><img src={twitter}className="App-links" /></a>
+            <a href="https://medium.com/@dssilvap"
+            target="_blank"
+            rel="noopener noreferrer" className="App-links" alt="medium"><img src={medium} className="App-links" /></a>
+          </div>
         </header>
       </div>
     );
