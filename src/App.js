f1fa9d6d105519cd3719c814e41cb8bc61ac190c
Davi Silva <davi@localhost.localdomain>
2019-02-20 23:39:25 -0300

    upgrade files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import github from './github.png';
 import twitter from './twitter.png';
-import medium from './medium.png';
+import medium from './medium.png'
 import './App.css';
-
 
 class App extends Component {
   render() {
@@ -15,17 +14,23 @@ class App extends Component {
             Hi, I'm Davi Silva
           </h3>
           <p>Welcome here in my personal Website.</p>
-        
+
           <div className="links">
             <a href="https://github.com/davi1985"
             target="_blank"
-            rel="noopener noreferrer" className="App-links" alt="github"><img src={github} className="App-links"/></a>
+            rel="noopener noreferrer" className="App-links" alt="github">
+                <img src={github} className="App-links"/>
+            </a>
             <a href="https://twitter.com/dssilvap"
             target="_blank"
-            rel="noopener noreferrer" className="App-links" alt="twitter"><img src={twitter}className="App-links" /></a>
+            rel="noopener noreferrer" className="App-links" alt="twitter">
+                <img src={twitter}className="App-links" />
+            </a>
             <a href="https://medium.com/@dssilvap"
             target="_blank"
-            rel="noopener noreferrer" className="App-links" alt="medium"><img src={medium} className="App-links" /></a>
+            rel="noopener noreferrer" className="App-links" alt="medium">
+                <img src={medium} className="App-links" />
+            </a>
           </div>
         </header>
       </div>
